9253fed845ce7cf2cc6a24b6c86db4eb44d7912b
Leonardo Jorge Tecco <teccoleo@hotmail.com>
2019-04-13 00:22:33 -0300

    Create Header and Add Todo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import Todos from './components/Todos';
+import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
+import uuid from 'uuid';
 
 import './App.css';
 
 class App extends Component {
   state = {
     todos: [
-      { id: 1, title: 'Take out the trash', completed: false },
-      { id: 2, title: 'Dinner with wife', completed: true },
-      { id: 3, title: 'Meeting with boos', completed: false }
+      { id: uuid.v4(), title: 'Take out the trash', completed: false },
+      { id: uuid.v4(), title: 'Dinner with wife', completed: true },
+      { id: uuid.v4(), title: 'Meeting with boos', completed: false }
     ]
   };
 
@@ -30,10 +33,30 @@ class App extends Component {
     });
   }
 
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid.v4(),
+      title,
+      completed: false
+    };
+
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+        <div className="container">
+          <Header />
+          <AddTodo addTodo={this.addTodo} />
+          <Todos
+            todos={this.state.todos}
+            markComplete={this.markComplete}
+            delTodo={this.delTodo}
+          />
+        </div>
       </div>
     );
   }
